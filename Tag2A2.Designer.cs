a5d0944cb08c8ab6b855964ac8186135e181319e
MAAHoogkamer <111110141+MAAHoogkamer@users.noreply.github.com>
2022-08-12 11:23:48 +0200

    Add files via upload

---
+++ Tag2A2.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace XMLtest
+{
+    partial class Tag2A2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadButt = new System.Windows.Forms.Button();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.saveButt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loadButt
+            // 
+            this.loadButt.Location = new System.Drawing.Point(616, 25);
+            this.loadButt.Name = "loadButt";
+            this.loadButt.Size = new System.Drawing.Size(172, 23);
+            this.loadButt.TabIndex = 0;
+            this.loadButt.Text = "Load settings XML in box";
+            this.loadButt.UseVisualStyleBackColor = true;
+            this.loadButt.Click += new System.EventHandler(this.loadButt_Click);
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(12, 54);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputBox.Size = new System.Drawing.Size(776, 384);
+            this.outputBox.TabIndex = 1;
+            // 
+            // saveButt
+            // 
+            this.saveButt.Location = new System.Drawing.Point(12, 25);
+            this.saveButt.Name = "saveButt";
+            this.saveButt.Size = new System.Drawing.Size(172, 23);
+            this.saveButt.TabIndex = 2;
+            this.saveButt.Text = "Save window location and size";
+            this.saveButt.UseVisualStyleBackColor = true;
+            this.saveButt.Click += new System.EventHandler(this.saveButt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(190, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = " ";
+            // 
+            // Tag2A2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveButt);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.loadButt);
+            this.Name = "Tag2A2";
+            this.Text = "Tag2A2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadButt;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Button saveButt;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Tag2A2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+//For using xml with linq:
+using System.Xml.Linq;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace XMLtest
+{
+    public partial class Tag2A2 : Form
+    {
+        //Load 'Main' code here:
+        public Tag2A2()
+        {
+            InitializeComponent();
+            createXML();
+        }
+        public static void createXML()
+        {
+            XDocument xDoc = new XDocument(
+                        new XDeclaration("1.0", "UTF-16", null),
+                            new XElement("Window_properties",
+                                new XComment("Window location:"),
+                                new XElement("WinLoc", ""),
+                                new XComment("Window size:"),
+                                new XElement("WinSize", "")
+                                ));
+            xDoc.Save(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "tempXMLtest.xml")
+            );
+        }
+        //Set the path for local app data:
+        static string localPath = Path.Combine
+        (
+         Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "tempXMLtest.xml"
+        );
+        //Loading the XML doc with Linq:
+        XDocument windowSet = XDocument.Load(localPath);
+        private void loadButt_Click(object sender, EventArgs e)
+        {
+            outputBox.Text += windowSet;
+        }
+        //Get form values and write them to doc:
+        private void saveButt_Click(object sender, EventArgs e)
+        {
+            var WinLoc = this.Location.ToString();
+            var WinSize = this.Size.ToString();
+            var winz = windowSet;
+            winz.XPathSelectElement("//Window_properties/WinLoc").Value = WinLoc;
+            winz.XPathSelectElement("//Window_properties/WinSize").Value = WinSize;
+            winz.Save(localPath);
+        }
+    }
+}
